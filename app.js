a8e4168f6bd3d96f64f5c079830924b8d8073d44
Bora Kim <borakim@boras-mbp.mynet>
2020-12-07 11:55:24 +0000

    Amended data types to object and gave an ID to each data added

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const { v4: uuidv4 } = require('uuid');
-//why not working??
-
-const wordH2Input = document.getElementById("japanese")
-const wordMeaningInput = document.getElementById("english")
-
+const newWords =
+    {
+        id : 0,
+        wordH2Input : document.getElementById("japanese"),
+        wordMeaningInput :document.getElementById("english")
+    };
+const resArr = [];
 // var newHtml = `<div class="card border-success m-3"  style="width: 18rem;"><div class="card-body"><h2 class="card-title">`+ wordH2Input.value+'</h2><p class="card-text">'+wordMeaningInput.value+'</p><button type="button" class="btn btn-outline-info">update</button><button type="button" class="btn btn-outline-danger">delete</button></div>'
 
-const html1 = '<div class="card border-success m-3"  style="width: 18rem;"><div class="card-body"><h2 class="card-title">'
+const html1 = '<div class="card border-success m-3" style="width: 18rem;"><div class="card-body"><h2 class="card-title">'
 const html2 = '</h2><p class="card-text">'
-const html3 = '</p><button type="button" class="btn btn-outline-info">update</button><button type="button" class="btn btn-outline-danger">delete</button></div>'
+const html3 = '</p><button type="button" class="btn btn-outline-info">update</button><button type="button" class="btn btn-outline-danger" id="%id%">delete</button></div>'
 
-
-document.querySelector("#add-button").addEventListener("click", addWord
-)
-
+document.querySelector("#add-button").addEventListener("click", addWord);
 function addWord (event){
     event.preventDefault();
     var d1 = document.getElementById('card');
-    d1.insertAdjacentHTML('beforeend', html1+wordH2Input.value+html2+wordMeaningInput.value+html3);
-    console.log("function working")
-    uuidv4();
+    d1.insertAdjacentHTML('beforeend', html1+newWords.wordH2Input.value+html2+newWords.wordMeaningInput.value+html3);
+    html1.replace('%id%', newWords.id);
+    newWords.id = Date.now()
+    resArr.push(newWords);
+
+    console.log("function is working" + newWords.id);
 }
 
-document.querySelector
+//We would need to figure out how to push the date ID to the created object and inject it to the below code 
 
+// document.querySelector('#'+).addEventListener("click", function(){
+//     console.log("clicked")
+// })
 
-var myobj = document.getElementById("demo");
-myobj.remove();
