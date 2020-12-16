c801d2a7956dc9cede1aec43d105482c5e8734fe
Bora Kim <borakim@boras-mbp.mynet>
2020-12-16 15:22:49 +0000

    Restructured functions for Add item, Edit and Update

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 - This website created to memorize Japanese word and we created this website for our passion to learn Japanese.
 - Use pure Javascript.
-- Add a word that you want to remember.
+- Feel free to add a word that you want to memorize. 
+- Once you've memorized it, click 'delete' button to delete the word. 
+- Any typo? don't worry. just click 'edit' button to amend any mistakes. 
--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const newWords ={
     wordH2Input : document.getElementById("japanese"),
     wordMeaningInput :document.getElementById("english")
 };
-
-const html1 = '<li class="card border-success m-3" style="width: 18rem;"><div class="card-body"><h2 class="card-title">'
-const html2 = '</h2><p class="card-text">'
-const html3 = '</p></div><button type="button" class="btn btn-outline-warning edit">edit</button><button type="button" class="btn btn-outline-info update">update</button><button type="button" class="btn btn-outline-danger delete">delete</button></li>'
+const html1 = '<li class="card border-success m-3" style="width: 18rem;"><div class="card-body"><input type="textbox" class="card-title" value="'
+const html2 = '" disabled></input><input type="textbox" class="card-text" value="'
+const html3 = '" disabled></input></div><div class="flex mb-3"><button type="button" class="btn btn-outline-warning edit">edit</button><button type="button" class="btn btn-outline-info update">update</button><button type="button" class="btn btn-outline-danger delete">delete</button></div></li>'
 const validate = document.querySelector('input');
 
 document.querySelector("#add-button").addEventListener("click", addWord);
 function addWord (event){
-if(validate.value === ""){
-    alert("Please insert your word of the day");
-} else {
-    event.preventDefault();
-    const d1 = document.getElementById('card');
-    d1.insertAdjacentHTML('beforeend', html1+newWords.wordH2Input.value+html2+newWords.wordMeaningInput.value+html3);
-    localStorage.setItem("word", newWords.wordH2Input.value);
-    document.getElementById("japanese").value = ""
-    document.getElementById("english").value = ""
-}
+    if(validate.value === ""){
+        alert("Please insert your word of the day");
+    } else {
+        event.preventDefault();
+        const d1 = document.getElementById('card');
+        d1.insertAdjacentHTML('beforeend', html1+newWords.wordH2Input.value+html2+newWords.wordMeaningInput.value+html3);
+        localStorage.setItem("word", newWords.wordH2Input.value);
+        document.getElementById("japanese").value = ""
+        document.getElementById("english").value = ""
+    }
 };
 const string1 = '<input class="update-input-box" type="text" placeholder="'
 const string2 = '"></input>'
@@ -27,33 +26,20 @@ const string2 = '"></input>'
 document.getElementById('card').addEventListener('click', function(e) {
 const tgt = e.target;
 const parent = tgt.closest('li');
+const japanese = parent.querySelector(".card-title")
+const english = parent.querySelector(".card-text")
+
 if (tgt.classList.contains('delete')) parent.remove();
-else if (tgt.classList.contains('edit')) {
-// console.log("Update clicked for ", parent.querySelector(".card-title").textContent)
-// document.createElement("input");
-    const japanese = parent.querySelector(".card-title")
-    const english = parent.querySelector(".card-text")
-    japanese.insertAdjacentHTML('beforeend', string1+japanese.textContent+string2);
-    english.insertAdjacentHTML('beforeend', string1+english.textContent+string2);
-    // parent.querySelector("card-title").textContent.style.display ="none"
-}
-else if (tgt.classList.contains('update')) {
-    // console.log("Update clicked for ", parent.querySelector(".card-title").textContent)
-    // document.createElement("input");
-    console.log("update")
-    console.log(document.getElementById("update-input-box").value)
-    //need to have a look
-    const editedWords ={
-        editedWordJapanese : document.getElementById("japanese"),
-        editedWordEnglish : document.getElementById("english")
-    };
+    else if (tgt.classList.contains('edit')) {
+        japanese.removeAttribute('disabled');        
+        english.removeAttribute('disabled');
+        japanese.style.border = "1px solid rgb(224, 140, 154)";
+        english.style.border = "1px solid rgb(224, 140, 154)";
+    }
+    else if (tgt.classList.contains('update')) {
+        japanese.setAttribute("disabled",'disabled');
+        english.setAttribute("disabled",'disabled');
+        japanese.style.border="none";
+        english.style.border="none";
     }
 });
-
-
-//<input type="text" placeholder="Jap"><h2 class="card-title">にほんご</h2></input>
-
-// editInput.type="text";
-// var editTask=function(){
-//     console.log("Edit Task...");
-//     console.log("Change 'edit' to 'save'");
